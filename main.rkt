262cb53b416a840525ea3a1e72fa5a1e30590e5d
t <witiwtyler@gmail.com>
2025-02-13 13:15:52 -0800

    Progressed and added more

--- main.rkt
+++ main.rkt
@@ -1,8 +1,7 @@
-
 ; Recursive Mandelbrot point generation
 ; Each time a set of points is recursively plotted and calculated,
-; the resulting set (z c) is added to a file, which is read by a python graphing script
-
+; the resulting set (z c) is added to a file, which is read by a python or graphing script or HTDP
+;
 ; Understanding the Mandelbrot Set
 ; The Mandelbrot Set works on complex numbers (a number consisting of two parts)
 ; Whether a point on the graph is inside the fractal or not depends on:
@@ -63,7 +62,7 @@
       ; x and y will be processed over within the bounds of -2 < x|y < 2
       ; Since x and y both start at -2.00, only bound we have to check is upper bound
 
-      (if (mandelbrot 0 x y 0 0) ; if the points are in the set (true)
+      (if (mandelbrot 0+0i x y 0) ; if the points are in the set (true)
 	 (begin
 	    (display "(")
 	    (display (+ x y))
@@ -81,28 +80,70 @@
 	 (if (= y (+ x 0+2.01i))
 	    ; if y == +0+2.00i: graphing is finished
 	    (display "Graphing is finished")
+
+	    ; else: 
 	    (begin
-	       ; else: move x back to the first column (-2.00) and move y down a row
+	       ; move x back to the first column (-2.00) and move y up a row
 	       (set! x -2.00)
 	       (mandelbrot-graph x (+ y 0+0.01i))
 	    )
-	    ; Graphing is done (graphed: -2.00-2.00i -> 2.00+2.00i)
 	 )
       )
-
    )
 )
 
 ; Will be returning true or false if the number is part of the set or not
 (define mandelbrot
-   (lambda (z cx cy acc count) 
+   (lambda (z cx cy counter) 
       ; At the end of 500 iterations of formula z = z^2 + c if z <= 2
       ; #t if zn <= 2
 
       ; cx = n
       ; cy = 0+ni
 
-      ()
+      ; doing: if (cx > 10000 or cx < -10000) or (cy > cx+10000i or cy < cx-10000i) the hard way 
+      (if (> cx 10000)
+	 ; if cx is out of positive bounds
+	 #f
+	 (display "") 
+      )
+      (if (< cx -10000)
+	 ; if cx is out of negative bounds
+	 #f
+	 (display "") 
+      )
+      (if (> cy (+ cx 0+10000i))
+	 ; if cy is out of positive bounds
+	 #f
+	 (display "") 
+      )
+      (if (< cy (+ cx 0-10000i))
+	 ; if cy is out of negative bounds
+	 #f
+	 (display "") 
+      )
+	 
+      ; Now that the checks are over, we can start calculating for z. Remember:
+      ; z0 =		0^2 + (0.4+0.1i) = 0.40+0.10i
+      ; z1 = (0.40+0.10i)^2 + (0.4+0.1i) = 0.55+0.18i
+      ; z2 = (0.55+0.18i)^2 + (0.4+0.1i) = 0.67+0.30i
+      ; z3 = (0.67+0.30i)^2 + (0.4+0.1i) = 0.76+0.50i
+      ; ...
+      ; ...
+      ; ...
+      ; zn > upper bound, therefore c = 0.4 + 0.1i is outside of the set
+       
+      ; if the count is at the final step and hasn't returned false yet
+      (if (= counter 500)
+	 #t
+	 ; Get the new z for the next iteration, increase counter, and recurse
+	 (begin
+	    (set! z (+ (expt z 2) (+ cx cy)))
+	    ; Rounding Complex Numbers: Revised Report Pg. 24 (imag-num real-num)
+	    (+ counter 1)
+	    (mandelbrot z cx cy counter)
+	 )
+      )
    )
 )
 
